9d219659cdd28e8ef7ccbfdd34da76190163c7e2
Joeltt <joelttm15@gmail.com>
2024-09-08 12:51:01 +0100

    feat: Add custom queries to AlbumRepository

--- src/main/java/com/northcoders/recordshopAPI/repository/AlbumRepository.java
+++ src/main/java/com/northcoders/recordshopAPI/repository/AlbumRepository.java
@@ -1,7 +1,18 @@
 package com.northcoders.recordshopAPI.repository;
 
-import com.northcoders.recordshopAPI.model.Album;
+import com.northcoders.recordshopAPI.model.AlbumModel;
+import com.northcoders.recordshopAPI.model.Genre;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
-public interface AlbumRepository extends CrudRepository<Album, Long> {
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface AlbumRepository extends CrudRepository<AlbumModel, Long> {
+    AlbumModel findByTitle(String title);
+    List<AlbumModel> findByArtist(String artist);
+    List<AlbumModel> findByGenre(Genre genre);
+    List<AlbumModel> findByReleasedYear(Integer year);
+
 }
