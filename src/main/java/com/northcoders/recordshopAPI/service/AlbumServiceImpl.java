78b357d7f5b6e952159bf2a5ff786af977242e23
Joeltt <joelttm15@gmail.com>
2024-09-11 10:48:52 +0100

    feat: Add custom findALlOrderByIdAsc() method

--- src/main/java/com/northcoders/recordshopAPI/service/AlbumServiceImpl.java
+++ src/main/java/com/northcoders/recordshopAPI/service/AlbumServiceImpl.java
@@ -23,15 +23,13 @@ public class AlbumServiceImpl implements AlbumService {
 
     @Override
     public List<AlbumModel> getAllAlbums() {
-        List<AlbumModel> albumList = new ArrayList<>();
-        albumRepository.findAll().forEach(albumList::add);
-        return albumList;
+        return new ArrayList<>(albumRepository.findAllByOrderByIdAsc());
     }
 
     @Override
     public List<AlbumModel> getAllAlbumsInStock() {
         List<AlbumModel> albumList = new ArrayList<>();
-        albumRepository.findAll().forEach(album -> {
+        albumRepository.findAllByOrderByIdAsc().forEach(album -> {
             if (album.getStock() != null && album.getStock() > 0) {
                 albumList.add(album);
             }
